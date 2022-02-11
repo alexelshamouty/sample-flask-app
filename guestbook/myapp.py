f3aa9fc2b04f3e7928920f403337d4c789a8d9d5
Alex Elshamouty <aelshamouty@gmail.com>
2022-02-11 16:04:07 +0100

    Adding some more code

---
+++ guestbook/myapp.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, url_for
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return render_template("templates/index.html")
+
+    @app.route("/guestbook")
+    def guestbook():
+        return "<h1> will be here in a bit </h1>"
+
+    return app
+
+if __name__ == "__main__":
+    application = create_app()
+    application.run()
