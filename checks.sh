b9625e70bb421591b3f72c2891ad1be8eddd2386
Alex Elshamouty <alex.elshamouty@gmail.com>
2022-02-12 11:38:15 +0100

    Update checks.sh
    Fixing checks scrip

--- checks.sh
+++ checks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Running checks before"
+echo "Running checks! Making sure repo is confirming to code quality"
+
+export PATH=$PATH:~./poetry/bin
 
 poetry run black --check guestbook
 poetry run isort --check guestbook
