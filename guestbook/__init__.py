d4a4a5a26955a67f47952ee055e21d7b500e66fe
Alex Elshamouty <aelshamouty@gmail.com>
2022-02-15 17:24:52 +0100

    Support sentry for application error tracing

--- guestbook/__init__.py
+++ guestbook/__init__.py
@@ -3,6 +3,28 @@ from os import environ
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=f"{SENTRY_DSN}",
+        integrations=[FlaskIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # By default the SDK will try to use the SENTRY_RELEASE
+        # environment variable, or infer a git commit
+        # SHA as release, however you may want to set
+        # something more human-readable.
+        # release="myapp@1.0.0",
+    )
+
 
 app = Flask(__name__)
 
