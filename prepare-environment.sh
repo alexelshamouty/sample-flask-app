fd8c6b978f4df39c379b530d942e8d01218bc5a4
Alex Elshamouty <alex.elshamouty@gmail.com>
2022-02-28 10:28:44 +0000

    Fixing a small error

--- prepare-environment.sh
+++ prepare-environment.sh
@@ -8,7 +8,7 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
-rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
+sudo rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
